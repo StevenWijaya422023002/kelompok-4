188d6720646aa1541bd7ea8b0901521b4da1f922
Yesagung <yesagung30@gmail.com>
2024-01-04 21:41:20 +0700

    fibonaci dan sentinel

---
+++ Searching/Sentinel_linear_search.py
@@ -0,0 +1,24 @@
+import time
+
+def sentinel_search(arr, target):
+    start_time = time.time()
+
+    arr.append(target)
+    i = 0
+    while arr[i] != target:
+        i += 1
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    if i < len(arr) - 1:
+        return f"Target {target} ditemukan pada indeks {i}\nWaktu eksekusi: {elapsed_time} detik"
+    else:
+        return f"Target {target} tidak ditemukan dalam array\nWaktu eksekusi: {elapsed_time} detik"
+
+# Contoh penggunaan
+array = [1, 2, 3, 4, 5]
+target_value = 3
+
+result = sentinel_search(array, target_value)
+print(result)
