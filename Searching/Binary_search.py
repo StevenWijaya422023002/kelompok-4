144b126ae101b9dc0dc9a588e9d1164262b2d808
StevenWijaya422023002 <steven.422023002@civitas.ukrida.ac.id>
2024-01-09 23:02:02 +0700

    Final Merge

---
+++ Searching/Binary_search.py
@@ -0,0 +1,30 @@
+import timeit
+
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2  
+
+        if arr[mid] == target:
+            return mid  
+        elif arr[mid] < target:
+            low = mid + 1  
+        else:
+            high = mid - 1  
+    return -1  
+
+my_sorted_list = [9, 6, 8, 7, 10, 15, 13, 22, 20 ]  
+target_value = 13
+
+start_time = timeit.default_timer()
+
+result = binary_search(my_sorted_list, target_value)
+
+end_time = timeit.default_timer()
+
+runtime = end_time - start_time
+
+print("Result:", result)
+print("Runtime: {:.6f} seconds".format(runtime))
+
--- Searching/Linear_search.py
+++ Searching/Linear_search.py
@@ -1,29 +1,22 @@
 import timeit
 
-def binary_search(arr, target):
-    low, high = 0, len(arr) - 1
-
-    while low <= high:
-        mid = (low + high) // 2  
-
-        if arr[mid] == target:
-            return mid  
-        elif arr[mid] < target:
-            low = mid + 1  
+def linear(data, cari):
+        for i in range(len(data)):
+            if cari == data[i]:
+                print("item ada :", i)
+                break
         else:
-            high = mid - 1  
-    return -1  
+            print("item tiada")
 
-my_sorted_list = [9, 6, 8, 7, 10, 15, 13, 22, 20 ]  
-target_value = 13
+data = [9, 6, 8, 7, 10, 15, 13, 22, 20 ]
+cari = 22
 
 start_time = timeit.default_timer()
 
-result = binary_search(my_sorted_list, target_value)
+result = linear(data, cari)
 
 end_time = timeit.default_timer()
 
 runtime = end_time - start_time
 
-print("Result:", result)
 print("Runtime: {:.6f} seconds".format(runtime))
